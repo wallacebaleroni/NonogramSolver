1e74e2591c94a70a3ade0eeceb9aff82ea2caa70
Wallace Baleroni <wbaleroni@muxi.com.br>
2020-08-11 09:49:58 -0300

    done

--- main.py
+++ main.py
@@ -1,87 +1,28 @@
-# lines = [[3], [3], [0], [0], [0]]
-# columns = [[0], [0], [0], [0], [0]]
-#
-# board = [['.', '.', '.', '.', '.'],
-#          ['.', '.', '.', '.', '.'],
-#          ['.', '.', '.', '.', '.'],
-#          ['.', '.', '.', '.', '.'],
-#          ['.', '.', '.', '.', '.']]
-#
-#
-# def is_solved():
-#     return False
-#
-#
-# def try_to_fill_line(i):
-#     minimum_filling = sum(lines[i]) + (len(lines[i]) - 1)
-#     if minimum_filling <= len(columns)//2:
-#         return False
-#     if len(lines[i]) == 1:
-#         padding = len(columns) - lines[i][0]
-#         start = padding
-#         end = len(columns) - padding - 1
-#         for j in range(start, end + 1):
-#             board[i][j] = 'O'
-#         return True
-#     return False
-#
-#
-# def evaluate_line_rec(line, k, values):
-#     if values[0] == 0:
-#         values.pop(0)
-#         if len(values) > 0:
-#             return evaluate_line_rec(line, k + 1, values)
-#         else:
-#             print(line)
-#             return values
-#     else:
-#         if board[i][k] != 'X':
-#             line[k] += 1
-#             values[0] -= 1
-#             evaluate_line_rec(line, k + 1, values)
-#             line[k] -= 1
-#             values[0] += 1
-#             evaluate_line_rec(line, k + 1, values)
-#         else:
-#             return values
-#
-#
-# def evaluate_line(i):
-#     line = len(columns) * [0]
-#     values = lines[i]
-#     k = 0
-#     if values[0] > 0:
-#         line[k] += 1
-#         values[0] -= 1
-#         evaluate_line_rec(line, k + 1, values)
-#
-#
-# altered = True
-# while altered:
-#     altered = False
-#     for i in range(len(lines)):
-#         altered = try_to_fill_line(i)
-#         evaluate_line(i)
-#     # para cada coluna
-#         # tentar preencher
-#     print(board)
-
-#
 import copy
 
-lines =   [[2], [2], [6], [4], [1,3], [6,3], [1,3,4], [8,5], [1,3,5], [6,4], [1,3,5]]
-columns = [[1], [3], [2,3], [3,5], [3,3,3], [1,3,2,6], [1,2,2,8], [2,3,2,5], [2,8], [3,6,1]]
+lines = [[5], [4], [3,3], [7,2], [8,2], [2,3,5], [10], [9,5], [11,3], [3,3,3,3],[2,5,3,2],[2,2,5,1],[1,2,2,3],[2,3,3],[3,2,2],[2,2,2],[2,1],[3],[3],[3]]
+columns = [[1],[3],[2,3], [3, 5], [3,3,3], [1, 3, 2,6], [1, 2, 2, 8], [2,3,2,5], [2, 8], [3,6,1],[4,5,3],[7,5],[5,3],[2,4],[3,5,2],[3,6],[3,3],[4],[5],[2]]
 
-start_board = [['.', '.', '.', '.', '.', '.', '.', '.', '.', '.'],
-               ['.', '.', '.', '.', '.', '.', '.', '.', '.', '.'],
-               ['.', '.', '.', '.', '.', '.', '.', '.', '.', '.'],
-               ['.', '.', '.', '.', '.', '.', '.', '.', '.', '.'],
-               ['.', '.', '.', '.', '.', '.', '.', '.', '.', '.'],
-               ['.', '.', '.', '.', '.', '.', '.', '.', '.', '.'],
-               ['.', '.', '.', '.', '.', '.', '.', '.', '.', '.'],
-               ['.', '.', '.', '.', '.', '.', '.', '.', '.', '.'],
-               ['.', '.', '.', '.', '.', '.', '.', '.', '.', '.'],
-               ['.', '.', '.', '.', '.', '.', '.', '.', '.', '.']]
+start_board = [['.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.'],
+               ['.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.'],
+               ['.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.'],
+               ['.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.'],
+               ['.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.'],
+               ['.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.'],
+               ['.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.'],
+               ['.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.'],
+               ['.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.'],
+               ['.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.'],
+               ['.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.'],
+               ['.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.'],
+               ['.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.'],
+               ['.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.'],
+               ['.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.'],
+               ['.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.'],
+               ['.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.'],
+               ['.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.'],
+               ['.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.'],
+               ['.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.']]
 
 
 def increment_heat(line_heat, line):
@@ -93,31 +34,36 @@ def increment_heat(line_heat, line):
 
 def is_solution_line(line, original_values):
     sequence_size = 0
-    values = original_values[:]
-    for j in range(len(lines)):
-        if line[j] == 'O':
+    values = []
+
+    for i in range(len(line)):
+        if line[i] == 'O':
             sequence_size += 1
         else:
-            if values[0] == sequence_size:
+            if sequence_size > 0:
+                values.append(sequence_size)
                 sequence_size = 0
-                values.pop(0)
-                if len(values) == 0:
-                    return True
-    if values[0] == sequence_size:
-        values.pop(0)
-        if len(values) == 0:
-            return True
-    return False
+    if sequence_size > 0:
+        values.append(sequence_size)
+
+    if len(values) != len(original_values):
+        return False
+
+    for i in range(len(values)):
+        if values[i] != original_values[i]:
+            return False
+
+    return True
 
 
 def fill_line(original_line, value, j):
     line = copy.deepcopy(original_line)
     for k in range(value):
-        line[j+k] = 'O'
+        line[j + k] = 'O'
     if j > 0:
-        line[j-1] = 'X'
-    if j+value < len(columns):
-        line[j+value] = 'X'
+        line[j - 1] = 'X'
+    if j + value < len(columns):
+        line[j + value] = 'X'
     return line
 
 
@@ -130,9 +76,15 @@ def fits_line(line, value, j):
         return False
     else:
         if value == 1:
-            return True
+            if j == len(line) - 1:
+                return True
+            else:
+                if line[j + 1] == 'O':
+                    return False
+                else:
+                    return True
         else:
-            return fits_line(line, value-1, j+1)
+            return fits_line(line, value - 1, j + 1)
 
 
 def evaluate_line_rec(line_heat, solutions, line, values, j_0, k):
@@ -144,7 +96,7 @@ def evaluate_line_rec(line_heat, solutions, line, values, j_0, k):
         return line_heat, solutions
     for j in range(j_0, len(line)):
         if fits_line(line, values[k], j):
-            line_heat, solutions = evaluate_line_rec(line_heat, solutions, fill_line(line, values[k], j), values, j, k+1)
+            line_heat, solutions = evaluate_line_rec(line_heat, solutions, fill_line(line, values[k], j), values, j + values[k] + 1, k + 1)
     return line_heat, solutions
 
 
@@ -154,10 +106,10 @@ def evaluate_line(line_heat, solutions, line, values):
     return line_heat, solutions
 
 
-def get_column(start_board, j):
+def get_column(board, j):
     column = []
-    for i in range(len(start_board)):
-        column.append(start_board[i][j])
+    for i in range(len(board)):
+        column.append(board[i][j])
     return column
 
 
