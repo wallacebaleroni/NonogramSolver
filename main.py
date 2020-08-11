ce40ed8c1d308652d11a12670a91840a5c1a9fb5
Wallace Baleroni <wbaleroni@muxi.com.br>
2020-08-11 10:59:34 -0300

    done and organized

--- main.py
+++ main.py
@@ -1,43 +1,28 @@
 import copy
 
-lines = [[5], [4], [3,3], [7,2], [8,2], [2,3,5], [10], [9,5], [11,3], [3,3,3,3],[2,5,3,2],[2,2,5,1],[1,2,2,3],[2,3,3],[3,2,2],[2,2,2],[2,1],[3],[3],[3]]
-columns = [[1],[3],[2,3], [3, 5], [3,3,3], [1, 3, 2,6], [1, 2, 2, 8], [2,3,2,5], [2, 8], [3,6,1],[4,5,3],[7,5],[5,3],[2,4],[3,5,2],[3,6],[3,3],[4],[5],[2]]
+MARKED = 'O'
+UNMARKED = 'X'
+CLEAR = '.'
 
-start_board = [['.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.'],
-               ['.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.'],
-               ['.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.'],
-               ['.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.'],
-               ['.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.'],
-               ['.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.'],
-               ['.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.'],
-               ['.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.'],
-               ['.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.'],
-               ['.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.'],
-               ['.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.'],
-               ['.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.'],
-               ['.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.'],
-               ['.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.'],
-               ['.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.'],
-               ['.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.'],
-               ['.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.'],
-               ['.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.'],
-               ['.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.'],
-               ['.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.']]
+rows = [[4],[4],[5],[3,4],[1,1,4],[1,4],[1,6],[2,4,1],[7],[7]]
+columns = [[5,1],[1,3],[2,2],[2,2],[3,4],[4,5],[10],[7],[5],[1,2]]
+
+board = [[CLEAR for x in range(len(columns))] for y in range(len(rows))]
 
 
 def increment_heat(line_heat, line):
     for i in range(len(line)):
-        if line[i] == 'O':
+        if line[i] == MARKED:
             line_heat[i] += 1
     return line_heat
 
 
-def is_solution_line(line, original_values):
+def is_line_solution(line, original_values):
     sequence_size = 0
     values = []
 
     for i in range(len(line)):
-        if line[i] == 'O':
+        if line[i] == MARKED:
             sequence_size += 1
         else:
             if sequence_size > 0:
@@ -56,53 +41,53 @@ def is_solution_line(line, original_values):
     return True
 
 
-def fill_line(original_line, value, j):
+def fill_value_in_line(original_line, value, j):
     line = copy.deepcopy(original_line)
     for k in range(value):
-        line[j + k] = 'O'
+        line[j + k] = MARKED
     if j > 0:
-        line[j - 1] = 'X'
+        line[j - 1] = UNMARKED
     if j + value < len(columns):
-        line[j + value] = 'X'
+        line[j + value] = UNMARKED
     return line
 
 
-def fits_line(line, value, j):
+def value_fits_in_line(line, value, j):
     if j >= len(line):
         return False
-    if len(lines) - j < value:
+    if len(rows) - j < value:
         return False
-    if line[j] == 'X':
+    if line[j] == UNMARKED:
         return False
     else:
         if value == 1:
             if j == len(line) - 1:
                 return True
             else:
-                if line[j + 1] == 'O':
+                if line[j + 1] == MARKED:
                     return False
                 else:
                     return True
         else:
-            return fits_line(line, value - 1, j + 1)
+            return value_fits_in_line(line, value - 1, j + 1)
 
 
-def evaluate_line_rec(line_heat, solutions, line, values, j_0, k):
-    if is_solution_line(line, values):
+def evaluate_line_recursive(line_heat, solutions, line, values, j_0, k):
+    if is_line_solution(line, values):
         solutions += 1
         line_heat = increment_heat(line_heat, line)
         return line_heat, solutions
     if k > len(values) - 1:
         return line_heat, solutions
     for j in range(j_0, len(line)):
-        if fits_line(line, values[k], j):
-            line_heat, solutions = evaluate_line_rec(line_heat, solutions, fill_line(line, values[k], j), values, j + values[k] + 1, k + 1)
+        if value_fits_in_line(line, values[k], j):
+            line_heat, solutions = evaluate_line_recursive(line_heat, solutions, fill_value_in_line(line, values[k], j), values, j + values[k] + 1, k + 1)
     return line_heat, solutions
 
 
 def evaluate_line(line_heat, solutions, line, values):
     for j in range(len(line)):
-        line_heat, solutions = evaluate_line_rec(line_heat, solutions, line, values, j, 0)
+        line_heat, solutions = evaluate_line_recursive(line_heat, solutions, line, values, j, 0)
     return line_heat, solutions
 
 
@@ -120,37 +105,44 @@ def main():
         changed = False
         loops_counter += 1
 
-        for i in range(len(start_board)):
-            line_heat = len(start_board[i]) * [0]
+        for i in range(len(rows)):
+            row_heat = len(board[i]) * [0]
             solutions = 0
-            line_heat, solutions = evaluate_line(line_heat, solutions, start_board[i], lines[i])
-            for k in range(len(line_heat)):
-                if line_heat[k] == solutions:
-                    if start_board[i][k] == '.':
-                        start_board[i][k] = 'O'
+            row_heat, solutions = evaluate_line(row_heat, solutions, board[i], rows[i])
+            for k in range(len(row_heat)):
+                if row_heat[k] == solutions:
+                    if board[i][k] == CLEAR:
+                        board[i][k] = MARKED
                         changed = True
-                elif line_heat[k] == 0:
-                    if start_board[i][k] == '.':
-                        start_board[i][k] = 'X'
+                elif row_heat[k] == 0:
+                    if board[i][k] == CLEAR:
+                        board[i][k] = UNMARKED
                         changed = True
 
-        for j in range(len(start_board[0])):
-            column_heat = len(start_board) * [0]
+        for j in range(len(columns)):
+            column_heat = len(board) * [0]
             solutions = 0
-            column_heat, solutions = evaluate_line(column_heat, solutions, get_column(start_board, j), columns[j])
+            column_heat, solutions = evaluate_line(column_heat, solutions, get_column(board, j), columns[j])
             for k in range(len(column_heat)):
                 if column_heat[k] == solutions:
-                    if start_board[k][j] == '.':
-                        start_board[k][j] = 'O'
+                    if board[k][j] == CLEAR:
+                        board[k][j] = MARKED
                         changed = True
                 elif column_heat[k] == 0:
-                    if start_board[k][j] == '.':
-                        start_board[k][j] = 'X'
+                    if board[k][j] == CLEAR:
+                        board[k][j] = UNMARKED
                         changed = True
 
-    print(loops_counter)
-    for i in range(len(start_board)):
-        print(start_board[i])
+    print("Loops necessary: " + str(loops_counter))
+    for i in range(len(rows)):
+        for j in range(len(columns)):
+            if board[i][j] == MARKED:
+                print(' O ', end='')
+            if board[i][j] == UNMARKED:
+                print(' . ', end='')
+            if board[i][j] == CLEAR:
+                print(' . ', end='')
+        print('', end='\n')
 
 
 main()
